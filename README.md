68aabb7f29b85a800be2f39d7641bbc5fa470666
Declan McGrath <declan@weuseopensource.com>
2021-03-31 00:32:39 +0100

    Updated reamde and tabs->spaces.

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # Ruby Top Tips - Volume 1
 All the top tips are listed in the following spec -
-[spec/lib/beans_catalog_spec.rb](https://github.com/theirishpenguin/ruby-top-tips-vol-1/blob/main/spec/lib/beans_catalog_spec.rb)
+[spec/lib/bean_catalog_spec.rb](https://github.com/theirishpenguin/ruby-top-tips-vol-1/blob/main/spec/lib/bean_catalog_spec.rb)
+
+Top Tip 1: Use hash_including() to ensure we call the service with the
+right "shape"
+
+Top Tip 2: Use a receive() with a block to debug the arguments
+
+Top Tip 3: Escape complex phrases in regular expressions
+
+Top Tip 4: When writing IF consider ELSE
+
+Top Tip 5: Similar to Top Tip 4 but for CASE statements
 
 So, take a look there to see them!
--- spec/lib/bean_catalog_spec.rb
+++ spec/lib/bean_catalog_spec.rb
@@ -6,71 +6,71 @@ require_relative '../../lib/legacy_bean_catalog_service'
 RSpec.describe BeanCatalog, type: :model do
 
   describe "#fetch_current_catalog" do
-		# Top Tip 1: Use hash_including() to ensure we call the service with the right "shape"
-		#
-		# This page on setting constraints on expectations in rspec is really valuable
-		# https://relishapp.com/rspec/rspec-mocks/docs/setting-constraints/matching-arguments
-		#
-		# This test should pass...
-		it "should call remote service to fetch data (passing example)" do
-			# Expect
-			expect(LegacyBeanCatalogService)
-			 .to receive(:get_list)
-			 .with(hash_including(provider: '3fe'))
+    # Top Tip 1: Use hash_including() to ensure we call the service with the right "shape"
+    #
+    # This page on setting constraints on expectations in rspec is really valuable
+    # https://relishapp.com/rspec/rspec-mocks/docs/setting-constraints/matching-arguments
+    #
+    # This test should pass...
+    it "should call remote service to fetch data (passing example)" do
+      # Expect
+      expect(LegacyBeanCatalogService)
+       .to receive(:get_list)
+       .with(hash_including(provider: '3fe'))
 
-			# Verify
-			BeanCatalog.fetch_current_catalog('3fe')
-		end
+      # Verify
+      BeanCatalog.fetch_current_catalog('3fe')
+    end
 
-		# This is similar to the last example but should fail...
-		it "should call remote service to fetch data (failing example)" do
-			# Expect
-			expect(LegacyBeanCatalogService)
-			 .to receive(:get_list)
-			 .with(hash_including(colour: 'blue'))
+    # This is similar to the last example but should fail...
+    it "should call remote service to fetch data (failing example)" do
+      # Expect
+      expect(LegacyBeanCatalogService)
+       .to receive(:get_list)
+       .with(hash_including(colour: 'blue'))
 
-			# Verify
-			BeanCatalog.fetch_current_catalog('3fe')
-			#
-			# ^^^ This should fail with...
-			#
-			# LegacyBeanCatalogService received :get_list with unexpected arguments
-			#  expected: (hash_including(:colour=>"blue"))
-			#  got: ({:provider=>"3fe"})
-			#
-			#  Diff:
-			#  @@ -1 +1 @@
-			#  -["hash_including(:colour=>\"blue\")"]
-			#  +[{:provider=>"3fe"}]
-		end
+      # Verify
+      BeanCatalog.fetch_current_catalog('3fe')
+      #
+      # ^^^ This should fail with...
+      #
+      # LegacyBeanCatalogService received :get_list with unexpected arguments
+      #  expected: (hash_including(:colour=>"blue"))
+      #  got: ({:provider=>"3fe"})
+      #
+      #  Diff:
+      #  @@ -1 +1 @@
+      #  -["hash_including(:colour=>\"blue\")"]
+      #  +[{:provider=>"3fe"}]
+    end
 
-		# Top Tip 2: Use a receive() with a block to debug the arguments.
-		#
-		# The arguments this example is a simple hash, but imagine if it was a large
-		# complex deeply nested hash. Being able to capture exactly what is going on
-		# in the call and debug it is much better than "guessing" levels of nesting,
-		# whether we are dealing with symbols or strings as keys, etc
-		#
-		# Tip found at https://github.com/rspec/rspec-mocks/issues/707#issuecomment-45551846
-		#
+    # Top Tip 2: Use a receive() with a block to debug the arguments.
+    #
+    # The arguments this example is a simple hash, but imagine if it was a large
+    # complex deeply nested hash. Being able to capture exactly what is going on
+    # in the call and debug it is much better than "guessing" levels of nesting,
+    # whether we are dealing with symbols or strings as keys, etc
+    #
+    # Tip found at https://github.com/rspec/rspec-mocks/issues/707#issuecomment-45551846
+    #
     # (This test should fail)
-		it "should call remote service to fetch data (debuggable example)" do
-			# Expect
-			expect(LegacyBeanCatalogService)
-			 .to receive(:get_list)
-			 .with(hash_including(colour: 'blue'))
+    it "should call remote service to fetch data (debuggable example)" do
+      # Expect
+      expect(LegacyBeanCatalogService)
+       .to receive(:get_list)
+       .with(hash_including(colour: 'blue'))
 
-			# Uncomment this to see what went wrong
-			# expect(LegacyBeanCatalogService)
-			#   .to receive(:get_list) do |*args|
-			#     require 'byebug' ; debugger
-			#     # You can even check simple asserts in here, such as...
-			#     expect(args[0][:provider]).to eql("3fe")
-			#   end
+      # Uncomment this to see what went wrong
+      # expect(LegacyBeanCatalogService)
+      #   .to receive(:get_list) do |*args|
+      #     require 'byebug' ; debugger
+      #     # You can even check simple asserts in here, such as...
+      #     expect(args[0][:provider]).to eql("3fe")
+      #   end
 
-			# Verify
-			BeanCatalog.fetch_current_catalog('3fe')
-		end
+      # Verify
+      BeanCatalog.fetch_current_catalog('3fe')
+    end
 
   end # of describe "#fetch_current_catalog"
 
@@ -145,13 +145,13 @@ RSpec.describe BeanCatalog, type: :model do
     end
 
     # Many months later someone considered negative integers...
-		# (This test should fail)
+    # (This test should fail)
     it "should double and normalise price for negative integer" do
       expect(BeanCatalog.get_doubled_normalised_price(-price_as_integer)).to eql("-€14.00")
     end
 
     # And of course, zero had always been a problem too...
-		# (This test should fail)
+    # (This test should fail)
     it "should double and normalise price for negative integer" do
       expect(BeanCatalog.get_doubled_normalised_price(0)).to eql("€0.00")
     end
